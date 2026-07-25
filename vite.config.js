52275a7addeabba5caa569c09475c4e9283876af
Joaquín Pino Suárez <pinosuarezjoaquin@gmail.com>
2026-07-25 10:49:46 +0200

    fix(ci): remove obsolete prop-types from vite optimizeDeps to unblock E2E

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
   optimizeDeps: {
-    include: ['prop-types', 'topojson-client'],
+    include: ['topojson-client'],
   },
   plugins: [
     react(),
